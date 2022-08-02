334d55c4e9586ccef462712a344034e170654043
mbss-devops <mubarak.baig25@gmail.com>
2022-08-02 15:36:15 +0530

    deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,3 +3,4 @@ new line is added from master branch
 this line is update from dev branch
 this file is moved to dev branch 
 this file contain deploy command 
+new line is adde to check stach command 
