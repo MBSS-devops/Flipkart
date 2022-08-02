2d66b0388874865141543a0b137da40438e6625f
mubarak_baig <73339506+mubarak-25@users.noreply.github.com>
2022-08-02 20:07:50 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,3 +4,4 @@ this line is update from dev branch
 this file is moved to dev branch 
 this file contain deploy command 
 new line is adde to check stach command 
+to test fetch command line is added 
